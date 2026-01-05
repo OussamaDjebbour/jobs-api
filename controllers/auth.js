038cf96c8cdb0f42bc48f5fc309b8fb353176e04
OussamaDjebbour <oussama_djebbour@outlook.com>
2026-01-05 14:04:42 +0100

    setup Mongoose pre save middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
-import bcryptjs from 'bcryptjs';
 import User from '../models/User.js';
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcryptjs.genSalt(10);
-
-  const hashedPassword = await bcryptjs.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
