7e309e667d30b4edbbe41a54154a7c67ece462c1
OussamaDjebbour <oussama_djebbour@outlook.com>
2026-01-06 14:52:30 +0100

    setup env variables, setup base login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
+import BadRequestError from '../errors/bad-request.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
 
 export const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -10,5 +12,19 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password ');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
